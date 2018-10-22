736351ca368ecbdb05ca3b8111b98a995dda9a16
sevrino <39475513+sevrino@users.noreply.github.com>
2018-10-22 16:41:41 +0900

    데이터베이스 연결문 추가

--- connect.py
+++ connect.py
@@ -1,1 +1,8 @@
+import MySQLdb
+db = MySQLdb.connect(host="localhost", user="root", passwd="root", db="banking")        
+cur = db.cursor()
+cur.execute("SELECT * FROM YOUR_TABLE_NAME")
+for row in cur.fetchall():
+    print row[0]
 
+db.close()
